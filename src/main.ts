7337d1a5e8f4276cbea76cb1f008556d051dcc3a
Denno2531 <dennis_ayo25@hotmail.com>
2025-02-21 19:44:56 -0500

    Validacion puerto
    Validacion puerto

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors();
+  
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
     forbidNonWhitelisted: true,
   }));
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Aplicación corriendo en el puerto ${port}`);
 }
 bootstrap();
